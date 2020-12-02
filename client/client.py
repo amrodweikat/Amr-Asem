45dd6cca0459e0218a482fc5fe1298363f41dcef
Your Name <you@example.com>
2020-12-02 16:22:16 +0200

    commit 4

--- client/client.py
+++ client/client.py
@@ -18,6 +18,9 @@ def lookup(number):
 	return  urllib.request.urlopen('http://127.0.0.1:5001/query_by_item/' + number).read()
 
 
+@app.route('/buy/<number>', methods=['POST'] )
+def buy():
+	return urllib.request.urlopen('http://127.0.0.1:5001/buy/' + number).read()
 
 
 if __name__ == '__main__':

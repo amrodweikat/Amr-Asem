b73d05b77fa7f3a15a7203ad3defb59eea81eddd
Your Name <you@example.com>
2020-12-04 13:54:22 +0200

    commit 9

--- client/client.py
+++ client/client.py
@@ -1,16 +1,30 @@
 
 import urllib
-
+import random
+import time
 from flask import Flask
+from flask_caching import Cache
 
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 app = Flask(__name__)
+cache.init_app(app)
+
+
+@cache.cached(timeout=120)
+def search_cache(word):
+	return  urllib.request.urlopen("http://127.0.0.1:5001/query_by_subject/" + word.replace(" ", "")).read() #called the query_by_subject function in catalog server
 
 
 
 @app.route('/search/<word>', methods=['GET'] )
 def search(word):
-	return  urllib.request.urlopen("http://127.0.0.1:5001/query_by_subject/" + word.replace(" ", "")).read() #called the query_by_subject function in catalog server
+	t1 = time.time()
+	w = search_cache(word)
+	t2 = time.time()
+	print("\ntime: "+str(t2-t1)+"\n")
+	return w
+	
 
 	 
 @app.route('/lookup/<number>', methods=['GET'] )
